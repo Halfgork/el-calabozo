76b8b1863c315fb43d2c247e4efae61a47507059
Görkem Şakalak <59141691+Halfgork@users.noreply.github.com>
2023-01-24 15:15:10 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,7 +9,7 @@ export const HERO = "hero2";
 export const SWORD = "sword2";
 
 /** Solana */
-export const PROGRAM_ID = new PublicKey("4tHDrx3PHLwLhFNhzTRw1UFWCmBrVkV1RrYpPxFVTDPD");
-export const TW_COLLECTION_ADDRESS ="G49RXHEyjT2W2BWKU7Cd2SA7idXwd7MsbDre3tpJ7dSM"
+export const PROGRAM_ID = new PublicKey("");
+export const TW_COLLECTION_ADDRESS =""
 import.meta.env.VITE_TW_COLLECTION_ADDRESS || "";
-export const NETWORK_URL = "https://solana-devnet.g.alchemy.com/v2/Sv2Zt1oE20HtYcHkl2sCbAcE76XkYPFV";
+export const NETWORK_URL = "";
